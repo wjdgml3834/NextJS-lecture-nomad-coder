7f27684d73d743bb8ee9f9e405e280a01d5ddde9
junghee.kim <wjdgml3834@naver.com>
2022-02-25 00:37:14 +0900

    📝 #1.1 Pages 폴더에 대하여

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
 export default function Home() {
-  return "hi";
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
 }
+
+// index는 우리 앱의 홈페이지로 연결시켜준다
+// jsx 똑같이 쓸수 있고 쓸 때 react 안불러와도 된다.
