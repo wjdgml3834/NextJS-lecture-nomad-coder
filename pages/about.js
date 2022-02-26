19ac8cc178e5eb02aed1992255eb373abfde6f0c
junghee.kim <wjdgml3834@naver.com>
2022-02-26 16:07:00 +0900

    🔥 다음강의 수강을 위한코드 삭제

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,3 @@
 export default function About() {
   return "about us~";
 }
-
-// url이름은 파일명이다.
-// 무조건 export default여야 한다. (아니면 에러남!)
-// 유저에게 보여주고 싶은게 있다면 pages 폴더에 export default function을 해야한다.
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,3 @@ export default function Home() {
     </div>
   );
 }
-
-// index는 우리 앱의 홈페이지로 연결시켜준다
-// jsx 똑같이 쓸수 있고 쓸 때 react 안불러와도 된다.
