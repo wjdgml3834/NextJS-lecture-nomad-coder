ebad90c30b78491eac905a1b1584b1ab8f7bda08
junghee.kim <wjdgml3834@naver.com>
2022-03-03 21:31:16 +0900

    🔥  다음강의 수강을 위한 코드 삭제

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,7 +3,6 @@ import { useRouter } from "next/router";
 
 export default function NavBar() {
   const router = useRouter();
-  console.log(router);
   return (
     <nav>
       <Link href="/">
@@ -17,8 +16,3 @@ export default function NavBar() {
     </nav>
   );
 }
-
-// a태그를 쓰면 전체 페이지를 새로고침하기에 의미가 없어진다. => 느리다.
-// useRouter는 일종의 훅인데 next에서 기본적으로 제공한다.
-// console.log(router) 해보면 우리 현재 location에 관한 정보들을 알 수 있다.
-// 잘은 모르겟지만, router.pathname은 url경로와 관련된 함수인거같다.
